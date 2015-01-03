d4adaf5e4acdf76c9a7405627c57853a6bbab916
Alexey Ivashin <ivashin.alexei@gmail.com>
2015-01-03 17:30:08 +0300

    introduce c-files with kernel code

--- prototype/cuda_prototype/cuda_operations.py
+++ prototype/cuda_prototype/cuda_operations.py
@@ -27,7 +27,7 @@ def make_boundary_conditions_for_potentials_2(phi, n, boundary_type='50_50_BOUND
                             # next_phi[i][j][k] = 0
                             next_phi[i][j][k] = phi
                         else:
-                            prev_phi[i][j][k] = 4*np.pi*ro*deltaT_
+                            prev_phi[i][j][k] = 1#4*np.pi*ro*deltaT_
                             next_phi[i][j][k] = 0
 
     return (prev_phi, next_phi)
@@ -57,70 +57,47 @@ def potential_establish_method(prev_phi, next_phi, ro, epsilon=0.01):
 
 
 def potential_establish_method_2(prev_phi, next_phi, ro, epsilon=0.01):
+    f = open('./kernel_code.c', 'r')
+    mod = SourceModule("".join(f.readlines()))
+    n = prev_phi.shape
+    step = np.array([xStepGrid, yStepGrid, zStepGrid], dtype=np.float32)
+    sizes = np.array(n, dtype=np.int32)
+    potential_establish = mod.get_function("potential_establish")
+    print(n[0],n[1],n[2])
+    gd = int((n[0]-2)*(n[1]-2)*(n[2]-2)/100)
+    if (gd != (n[0]-2)*(n[1]-2)*(n[2]-2)/100):
+        gd += 1
 
-    mod = SourceModule("""
+    bd = int((n[0]-2)*(n[1]-2)*(n[2]-2)/gd)
+    if (bd != (n[0]-2)*(n[1]-2)*(n[2]-2)/gd):
+        bd += 1
+    # gd, bd = 25, 390
+    print('gd={}, bd = {} ===================='.format(gd, bd))
 
-    __device__ __host__ float dif(float x, float y, float z, float step){
-        return (x - 2 * y + z) / step / step;
-    }
+    prev_sum, next_sum = epsilon, 3 * epsilon
+    while np.abs(prev_sum - next_sum) > epsilon:
+        prev_sum, next_sum = 0, 0
+        potential_establish(
+            drv.InOut(prev_phi), drv.InOut(next_phi), drv.In(step), drv.In(sizes), 
+            block=(bd,1, 1), grid=(gd,1))
+        for i in range(1, n[0] - 1):
+            for j in range(1, n[1] - 1):
+                for k in range(1, n[2] - 1):
+                    prev_sum += prev_phi[i][j][k]
+                    next_sum += next_phi[i][j][k]
+        # for kk in range(prev_phi.shape[0]):
+        #     print(next_phi[kk])
+            # plt.contourf(next_phi[kk])
+            # plt.show()
+        print('{}>{}'.format(np.abs(prev_sum - next_sum), epsilon))
+        # for kk in range(next_phi.shape[0]):
+        #     print(next_phi[kk])
 
-    //TODO change step and matrix_size to vector
-    __global__ void potential_establish(float* prev_phi, float* next_phi, float* step, int* matrix_size )
-    {
-        const int x_dim = matrix_size[0];
-        const int y_dim = matrix_size[1];
-        const int z_dim = matrix_size[2];
+    return (prev_phi, next_phi)
 
-        const int index = blockIdx.x*blockDim.x + threadIdx.x;
-        if (index >=(x_dim-2)*(y_dim-2)*(z_dim-2)) 
-            return;
-        const int i = (int) (index/(z_dim-2)/(y_dim-2))+1;
-        const int j = ((int) (index/(z_dim-2)))%(y_dim-2)+1;
-        const int k = index%(z_dim-2)+1;
-        // const int i = (int) (threadIdx.x/(z_dim-2)/(y_dim-2))+1;
-        // const int j = ((int) (threadIdx.x/(z_dim-2)))%(y_dim-2)+1;
-        // const int k = threadIdx.x%(z_dim-2)+1;
-
-        const float x_step = step[0];
-        const float y_step = step[1];
-        const float z_step = step[2];
-
-        const int ijk = i*z_dim*y_dim + j*z_dim + k;
-        const int im1jk = (i-1)*z_dim*y_dim + j*z_dim + k;
-        const int ijm1k = i*z_dim*y_dim + (j-1)*z_dim + k;
-        const int ijkm1 = i*z_dim*y_dim + j*z_dim + k - 1;
-        //const int ijkm1 = ijk - 1;
-        const int ip1jk = (i+1)*z_dim*y_dim + j*z_dim + k;
-        const int ijp1k = i*z_dim*y_dim + (j+1)*z_dim + k;
-        const int ijkp1 = i*z_dim*y_dim + j*z_dim + k + 1;
-        //const int ijkp1 = ijk + 1;
-
-        //for(int l=0; l <= 1000000; l++) 
-        float tmp = prev_phi[ijk];
-        float ro = 1E-17;
-        //for(int l=0; l <= 500; l++) 
-        {
-            prev_phi[ijk] = next_phi[ijk];
-            //TODO rewrite PI and deltaT_
-            //next_phi[i][j][k] = 1.75E-10 * (dif(prev_phi[i + 1][j][k], prev_phi[i][j][k], prev_phi[i - 1][j][k], x_step) + dif(prev_phi[i][j + 1][k], prev_phi[i][j][k], prev_phi[i][j - 1][k], y_step) + dif(prev_phi[i][j][k + 1], prev_phi[i][j][k], prev_phi[i][j][k - 1], z_step) + 4 * 3.1415926 * ro[i][j][k]) + prev_phi[i][j][k];
-            float tmp = 1.75E-10 * (
-            dif(prev_phi[ip1jk], prev_phi[ijk], prev_phi[im1jk], x_step) +
-            dif(prev_phi[ijp1k], prev_phi[ijk], prev_phi[ijm1k], y_step) +
-            dif(prev_phi[ijkp1], prev_phi[ijk], prev_phi[ijkm1], z_step) +
-            4 * 3.1415926 * ro) + prev_phi[ijk];
-            //tmp = dif(prev_phi[ip1jk], prev_phi[ijk], prev_phi[im1jk], x_step);
-            __syncthreads();
-            //TODO change this append to another where not depend to next 
-            //next_phi[ijk] = tmp;
-            //next_phi[ijk] = ijk + 1000*ijkm1 + 1000000*ijkp1;
-            //next_phi[ijk] = 1000*ijk + 1000000*ijkm1 + ijkp1;
-            next_phi[ijk] = 1000*next_phi[ijk] + 1000000*next_phi[ijkm1]
-             + next_phi[ijkp1];
-            //next_phi[ijk] = 100*ijk + 10000*ijm1k + ijp1k;
-            //next_phi[ijk] = next_phi[ijk] + 1;
-        }
-    } 
-    """)
+def potential_establish_method_3(prev_phi, next_phi, ro, epsilon=0.01):
+    f = open('./kernel_code_one_block.c', 'r')
+    mod = SourceModule("".join(f.readlines()))
     n = prev_phi.shape
     step = np.array([xStepGrid, yStepGrid, zStepGrid], dtype=np.float32)
     sizes = np.array(n, dtype=np.int32)
@@ -133,7 +110,9 @@ def potential_establish_method_2(prev_phi, next_phi, ro, epsilon=0.01):
     bd = int((n[0]-2)*(n[1]-2)*(n[2]-2)/gd)
     if (bd != (n[0]-2)*(n[1]-2)*(n[2]-2)/gd):
         bd += 1
-    print(gd, bd)
+    gd, bd = 1, 512
+    print('gd={}, bd = {} ===================='.format(gd, bd))
+
     prev_sum, next_sum = epsilon, 3 * epsilon
     while np.abs(prev_sum - next_sum) > epsilon:
         prev_sum, next_sum = 0, 0
@@ -145,17 +124,16 @@ def potential_establish_method_2(prev_phi, next_phi, ro, epsilon=0.01):
                 for k in range(1, n[2] - 1):
                     prev_sum += prev_phi[i][j][k]
                     next_sum += next_phi[i][j][k]
-        for kk in range(prev_phi.shape[0]):
-            print(next_phi[kk])
+        # for kk in range(prev_phi.shape[0]):
+        #     print(next_phi[kk])
             # plt.contourf(next_phi[kk])
             # plt.show()
         print('{}>{}'.format(np.abs(prev_sum - next_sum), epsilon))
-        # break
-        # print('===================')
         # for kk in range(next_phi.shape[0]):
         #     print(next_phi[kk])
 
     return (prev_phi, next_phi)
+
 
 def main():
     # Границы сетки
@@ -165,6 +143,9 @@ def main():
     # Граничные условия для потенциала
     n = (xRange.shape[0]+2, yRange.shape[0]+2, zRange.shape[0]+2)
     prev_phi, next_phi = make_boundary_conditions_for_potentials_2(50, n)
+    for i in range(prev_phi.shape[0]):
+        print(prev_phi[i])
+
     n = prev_phi.shape
     ro = np.zeros([n[0], n[1], n[2]], dtype=np.float32)
     # for i in range(1, n[0] - 1):
@@ -172,12 +153,14 @@ def main():
     #         for k in range(1, n[2] - 1):
     #             ro[i][j][k] = chargeGridElectron[i-1][j-1][k-1] + chargeGridCarbon[i-1][j-1][k-1] + chargeGridHelium[i-1][j-1][k-1]
     print('THIS IS START OF potential_establish_method_2')
-    prev_phi, next_phi = potential_establish_method_2(prev_phi, next_phi, ro, epsilon=0.01)
+    prev_phi, next_phi = potential_establish_method_3(prev_phi, next_phi, ro, epsilon=0.01)
     print('THIS IS END OF potential_establish_method_2')
 
     for i in range(next_phi.shape[0]):
-        print(next_phi[i])
-        plt.contourf(next_phi[i])
+        # print(next_phi[i])
+        cs = plt.contourf(next_phi[i])#, levels=[0, 10, 20, 30, 40, 45, 46, 47, 48, 49, 50, 51, 52])
+        print(cs.levels)
+        plt.colorbar(cs)
         plt.show()
 
 if __name__ == '__main__':
---
+++ prototype/cuda_prototype/kernel_code_one_block.c
@@ -0,0 +1,70 @@
+#include <math.h>
+__device__ __host__ double dif(float x, float y, float z, float step)
+{
+    return (double)((x - y - y + z) / step / step);
+}
+
+//TODO change step and matrix_size to vector
+__global__ void potential_establish(float *prev_phi, float *next_phi, float *step, int *matrix_size )
+{
+    const int x_dim = matrix_size[0];
+    const int y_dim = matrix_size[1];
+    const int z_dim = matrix_size[2];
+
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    // if (index >= (x_dim - 2) * (y_dim - 2) * (z_dim - 2))
+    //     return;
+    int i = (int) (index / (z_dim - 2) / (y_dim - 2)) + 1;
+    int j = ((int) (index / (z_dim - 2))) % (y_dim - 2) + 1;
+    int k = index % (z_dim - 2) + 1;
+
+    const float x_step = step[0];
+    const float y_step = step[1];
+    const float z_step = step[2];
+
+    int ijk = i * z_dim * y_dim + j * z_dim + k;
+    int im1jk = (i - 1) * z_dim * y_dim + j * z_dim + k;
+    int ijm1k = i * z_dim * y_dim + (j - 1) * z_dim + k;
+    int ijkm1 = i * z_dim * y_dim + j * z_dim + k - 1;
+    //int ijkm1 = ijk - 1;
+    int ip1jk = (i + 1) * z_dim * y_dim + j * z_dim + k;
+    int ijp1k = i * z_dim * y_dim + (j + 1) * z_dim + k;
+    int ijkp1 = i * z_dim * y_dim + j * z_dim + k + 1;
+    //int ijkp1 = ijk + 1;
+    float tmp = prev_phi[ijk];
+    float ro = 1E-17;
+    float precision = 0;
+    const int limit = 0;//(x_dim - 2);
+    //for (int l = 0; l <= 5; l++)
+    {
+        index = blockIdx.x * blockDim.x + threadIdx.x;
+        //for (int l = 0; l<20; l++)
+         while (index < limit)
+        {
+            // i = (int) (index / (z_dim - 2) / (y_dim - 2)) + 1;
+            // j = ((int) (index / (z_dim - 2))) % (y_dim - 2) + 1;
+            // k = index % (z_dim - 2) + 1;
+
+            // ijk = i * z_dim * y_dim + j * z_dim + k;
+            // im1jk = (i - 1) * z_dim * y_dim + j * z_dim + k;
+            // ijm1k = i * z_dim * y_dim + (j - 1) * z_dim + k;
+            // ijkm1 = i * z_dim * y_dim + j * z_dim + k - 1;
+            // ip1jk = (i + 1) * z_dim * y_dim + j * z_dim + k;
+            // ijp1k = i * z_dim * y_dim + (j + 1) * z_dim + k;
+            // ijkp1 = i * z_dim * y_dim + j * z_dim + k + 1;
+
+            // tmp = prev_phi[ijk];
+            // prev_phi[ijk] = tmp;
+            precision = (prev_phi[ip1jk] - prev_phi[ijk] - prev_phi[ijk] + prev_phi[im1jk]) / x_step / x_step +
+                        (prev_phi[ijp1k] - prev_phi[ijk] - prev_phi[ijk] + prev_phi[ijm1k]) / y_step / y_step +
+                        (prev_phi[ijkp1] - prev_phi[ijk] - prev_phi[ijk] + prev_phi[ijkm1]) / z_step / z_step +
+                        4 * M_PI * ro;
+            tmp = (float)(1.75E-10 * (precision) + prev_phi[ijk]);
+            // tmp = 1;
+            __syncthreads();
+            next_phi[ijk] = tmp;
+            prev_phi[ijk] = tmp;
+            index += 512;
+        }
+    }
+}
